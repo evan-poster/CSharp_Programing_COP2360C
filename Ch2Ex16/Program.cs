117c89d9cc2f56122d8bfb6263c8b17ae76441a3
evan-poster <evan.poster@gmail.com>
2024-09-09 10:45:51 -0400

    Completed Months Exercise (Ch2Ex16)

--- Ch2Ex16/Program.cs
+++ Ch2Ex16/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+// Create an enumeration named Month that holds values for the months of the year,
+// starting with JANUARY equal to 1. Write a program named MonthNames that prompts
+// the user for a month integer. Convert the user’s entry to a Month value, and display it.
+
+class Program
+{
+	public enum Month { JANUARY = 1, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER };
+
+	static void Main(string[] args)
+	{
+		WriteLine("Enter a number from 1 to 12: ");
+		int month = int.Parse(ReadLine());
+
+		WriteLine($"You entered {(Month)month}");
+	}
+}
