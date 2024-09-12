44527cafdf6b8e9ce65888f8405681e6ad3aa70d
evan-poster <evan.poster@gmail.com>
2024-09-12 09:56:49 -0400

    Ch4Ex2 project added

---
+++ Ch4Ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+// Accepts a user’s message and determines whether it is short enough
+// for a social networking service that does not accept messages of
+// more than 140 characters.
+
+namespace Ch4Ex1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Write("Enter a message: ");
+			string userMessage = ReadLine();
+
+			if (userMessage.Length > 140 || userMessage.Length == 0) {
+				WriteLine("Message not accepted.");
+			} else {
+				WriteLine("Message accepted.");
+			}
+		}
+	}
+}
