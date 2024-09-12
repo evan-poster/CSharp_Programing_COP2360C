2016cb658a8d9ef55c0471d1bcfca539edd8f7e3
evan-poster <evan.poster@gmail.com>
2024-09-12 09:19:51 -0400

    CanYouVote project added

--- CanYouVote/Program.cs
+++ CanYouVote/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+// C# program to check if a person can vote or not
+// Class exercise from 9/9/2024
+
+class CanYouVote {
+	static void Main() {
+		WriteLine("Enter your age: ");
+		int age = int.Parse(ReadLine());
+
+		WriteLine("Are you a citizen of the United States? (y/n) ");
+		string citizen = ReadLine().ToLower();
+
+		if ((citizen == "y") && (age >= 18)) {
+			WriteLine("You can vote.");
+		} else {
+			WriteLine("You can't vote.");
+		}
+	}
+}
