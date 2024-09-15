190a6145738d34acb1846ca6044eb6247d239efc
evan-poster <evan.poster@gmail.com>
2024-09-15 17:21:51 -0400

    Ch5Ex3 project added

---
+++ Ch5Ex3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+/*
+Write an application named EnterUppercaseLetters that asks the user to type an
+uppercase letter from the keyboard. If the character entered is an uppercase letter,
+display OK; if it is not an uppercase letter, display an error message. The program
+continues until the user types an exclamation point.
+*/
+
+namespace Ch5Ex3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool running = true;
+
+			while (running) {
+				Write("Enter an uppercase letter (! to quit): ");
+				char letter = char.Parse(ReadLine());
+				if (char.IsUpper(letter)) {
+					WriteLine("OK");
+				} else {
+					if (letter == '!') {
+						running = false;
+					} else {
+						WriteLine("Error");
+					}
+				}
+			}
+		}	
+	}
+}
