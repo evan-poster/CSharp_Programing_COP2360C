b89dfd0f838bbdd0eecc5e400d6066e5f5a4e0f7
evan-poster <evan.poster@gmail.com>
2024-09-16 12:46:44 -0400

    DutchFlags project added (incomplete)

---
+++ DutchFlags/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace DutchFlags
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			char[] flags = { 'D', 'F', 'D', 'G', 'D', 'F' };
+			// expected output: {'D', 'D', 'D', 'F', 'G'}
+		}
+	}
+}
