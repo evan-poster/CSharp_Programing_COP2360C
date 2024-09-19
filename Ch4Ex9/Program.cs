d1d0e77dbb49e647a77a30ada8eec26412db8510
evan-poster <evan.poster@gmail.com>
2024-09-19 08:58:44 -0400

    Ch4Ex9 updated; changed counting method

--- Ch4Ex9/Program.cs
+++ Ch4Ex9/Program.cs
@@ -33,11 +33,11 @@ namespace Ch4Ex9
 
 			// Generate three random numbers
 			Random random = new Random();
-			int [] winningNumbers = {random.Next(1, 5), random.Next(1, 5), random.Next(1, 5)};
+			int [] winningNumbers = [random.Next(1, 5), random.Next(1, 5), random.Next(1, 5)];
 
 			// Allow the user to guess three numbers
 			WriteLine("Enter 3 numbers between 1 and 4:");
-			int [] userNumbers = {int.Parse(ReadLine()), int.Parse(ReadLine()), int.Parse(ReadLine())};
+			int [] userNumbers = [int.Parse(ReadLine()), int.Parse(ReadLine()), int.Parse(ReadLine())];
 
 			// Check for three matching numbers, in order
 			if (userNumbers[0] == winningNumbers[0] && userNumbers[1] == winningNumbers[1] && userNumbers[2] == winningNumbers[2])
@@ -46,26 +46,38 @@ namespace Ch4Ex9
 				matches = 3;
 				WriteLine("JACKPOT!!!");
 			} else {
-				int [] userCounts = {
-					userNumbers.Count(n => n == 1),
-					userNumbers.Count(n => n == 2),
-					userNumbers.Count(n => n == 3),
-					userNumbers.Count(n => n == 4)
-				};
+				// Old Method
 
-				int [] winningCounts = {
-					winningNumbers.Count(n => n == 1),
-					winningNumbers.Count(n => n == 2),
-					winningNumbers.Count(n => n == 3),
-					winningNumbers.Count(n => n == 4)
-				};
+				// int [] userCounts = {
+				// 	userNumbers.Count(n => n == 1),
+				// 	userNumbers.Count(n => n == 2),
+				// 	userNumbers.Count(n => n == 3),
+				// 	userNumbers.Count(n => n == 4)
+				// };
 
-				int [] finalCount = {
+				// int [] winningCounts = {
+				// 	winningNumbers.Count(n => n == 1),
+				// 	winningNumbers.Count(n => n == 2),
+				// 	winningNumbers.Count(n => n == 3),
+				// 	winningNumbers.Count(n => n == 4)
+				// };
+
+				// Slightly better method
+
+				int [] userCounts = new int[4];
+				int [] winningCounts = new int[4];
+
+				for(int i = 0; i < winningNumbers.Length; i++) {
+					userCounts[userNumbers[i]-1]++;
+					winningCounts[winningNumbers[i]-1]++;
+				}
+
+				int [] finalCount = [
 					int.Min(userCounts[0], winningCounts[0]),
 					int.Min(userCounts[1], winningCounts[1]),
 					int.Min(userCounts[2], winningCounts[2]),
 					int.Min(userCounts[3], winningCounts[3])
-				};
+				];
 
 				// The sum of our finalCount array is our total matching numbers
 				int sum = finalCount.Sum();
@@ -81,7 +93,7 @@ namespace Ch4Ex9
 				}
 				matches = sum;				
 			}
-
+				
 			// Display user's award
 			WriteLine($"Winning ticket:\t{winningNumbers[0]}, {winningNumbers[1]}, {winningNumbers[2]}");
 			WriteLine($"Your guess:\t{userNumbers[0]}, {userNumbers[1]}, {userNumbers[2]}");
