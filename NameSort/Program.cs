d4ed36c49f6303bbd073fb226931e8714c86ec75
evan-poster <evan.poster@gmail.com>
2024-09-19 09:00:19 -0400

    NameSort misc exercise added

---
+++ NameSort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace NameSort
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			WriteLine(SortNames("John", "Mary", "Jane", "Bob"));
+		}
+
+		static string SortNames(params string[] names)
+		{
+			Array.Sort(names);
+			return string.Join(", ", names);
+		}
+	}
+}
