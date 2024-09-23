1d3d4f25abc2d955cb2165d46ac7cb5290e8443c
evan-poster <evan.poster@gmail.com>
2024-09-22 20:37:41 -0400

    Ch7Ex4 finished

--- Ch7Ex4/Program.cs
+++ Ch7Ex4/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+/*
+Create an application named ConvertMilesToKilometers whose Main() method
+prompts a user for a number of miles, passes the value to a method that converts
+the value to kilometers, and then returns the value to the Main() method where it is
+displayed. A mile is 1.60934 kilometers.
+*/
+
+class Program {
+	static void Main() {
+		// Prompt the user for a number of miles
+		WriteLine("Enter a number of miles: ");
+
+		// Convert the value to kilometers
+		double miles = double.Parse(ReadLine());
+
+		// Display the result
+		WriteLine($"{miles} miles is {ConvertMilesToKilometers(miles)} kilometers.");	
+	}
+
+	public static double ConvertMilesToKilometers(double miles) {
+		return miles * 1.60934;
+	}
+}
